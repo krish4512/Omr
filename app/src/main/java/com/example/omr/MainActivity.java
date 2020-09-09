1c63371e3b38aacfd972d93030350317bec84350
krish4512 <krish76.kp@gmail.com>
2020-09-09 16:59:21 +0530

    Splash Screen Handler and Delay added with 2 second time and jump to main activity.

--- app/src/main/java/com/example/omr/MainActivity.java
+++ app/src/main/java/com/example/omr/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.omr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
