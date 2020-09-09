008d42ce0d6c8cd25a1653ffd721bef53f41f44f
Krish <panchalkrishp@gmail.com>
2020-09-10 00:57:01 +0530

    Latest Updated.

--- app/src/main/java/com/example/omr/MainActivity.java
+++ app/src/main/java/com/example/omr/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FullScreen f = new FullScreen(this);
+
         setContentView(R.layout.activity_main);
     }
 }
